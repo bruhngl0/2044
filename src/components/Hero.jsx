3fa133fee67340287dfb4510ff1a7b7a44be9c88
bruhngl0 <adityashrm500@gmail.com>
2025-10-11 16:43:46 +0530

    mobi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,25 @@ import "../styles/hero.scss";
 const HeroSection = () => {
   return (
     <section className="hero">
-      <picture className="hero__image">
-        {/* Mobile image */}
-        <source srcSet="mobileCover.jpg" media="(max-width: 768px)" />
-        {/* Desktop image */}
-        <img src="webCover.jpg" alt="Hero" />
-      </picture>
+      <video
+        className="hero__video"
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster="fallback.jpg"
+        style={{
+          width: "100vw",
+        }}
+      >
+        <source src="vidwoo.mp4" type="video/mp4" />
+        <source src="vidwoo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
       <div className="hero__overlay">
         <div className="hero__content">
-          <h1>Twenty44</h1>
+          <h1>TWENTY44</h1>
           <p>More than a game. A way of living.</p>
           <button className="hero__cta">Explore</button>
         </div>
